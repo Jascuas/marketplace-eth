6dd33e16606339fae6891f6c8a655fc98189cc70
jascuas <loljavierdelago@hotmail.com>
2022-01-08 17:25:29 +0100

    api eth price

--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -2,31 +2,44 @@
 import { CourseList, CourseCard } from "@components/ui/course"
 import { BaseLayout } from "@components/ui/layout"
 import { getAllCourses } from "@content/courses/fetcher"
-import { WalletBar } from "@components/ui/web3"
+import { EthRates, WalletBar } from "@components/ui/web3"
 import { useAccount, useNetwork } from "@components/hooks/web3"
 import { Button } from "@components/ui/common"
 import { OrderModal } from "@components/ui/order"
+import { useState } from "react"
+import { useEthPrice } from "@components/hooks/useEthePrice"
 
 
 export default function Marketplace({ courses }) {
+    const [selectedCourse, setSelectedCourse] = useState(null);
     const { account } = useAccount()
     const { network } = useNetwork()
-
+    const {data} = useEthPrice()
     return (
         <>
             <WalletBar address={account.data} network={{ data: network.data, target: network.target, isSupported: network.isSupported }} />
+            {data &&
+                <EthRates data={data}/>
+            }
             <CourseList courses={courses}>
                 {
                     course =>
                         <CourseCard
                             key={course.id}
                             course={course}
-                            Footer={ () => 
-                                <div><Button variant="lightPurple">Purchase</Button></div>
+                            Footer={() =>
+                                <div><Button
+                                    onClick={() => setSelectedCourse(course)}
+                                    variant="lightPurple">
+                                    Purchase
+                                </Button></div>
                             } />
                 }
             </CourseList>
-            <OrderModal />
+            { selectedCourse &&
+                <OrderModal course={selectedCourse} onClose={() => setSelectedCourse(null)}/>
+            }
+            
         </>
     )
 }
