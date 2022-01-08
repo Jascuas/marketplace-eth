ddca5b1aec2eba5ca4e6021aabcd550ab3a94fa0
jascuas <loljavierdelago@hotmail.com>
2022-01-08 20:08:21 +0100

    active links

--- components/ui/common/breadcrumbs/index.js
+++ components/ui/common/breadcrumbs/index.js
@@ -1,22 +1,16 @@
-import Link from "next/link";
 
+import { ActiveLink } from "@components/ui/common/";
 
-
-export default function Breadcrumbs() {
-
+export default function Breadcrumbs({ items }) {
+ 
   return (
     <nav aria-label="breadcrumb" className=" flex justify-center py-4 px-4 sm:px-6 lg:px-8">
       <ol className="flex leading-none text-indigo-600 divide-x divide-indigo-400">
-        <Link href="#">
-          <a className="font-medium px-4 text-gray-500 hover:text-gray-900">Buy</a>
-        </Link>
-        <Link href="#">
-          <a className="font-medium px-4 text-gray-500 hover:text-gray-900">My Orders</a>
-        </Link>
-        <Link href="#">
-          <a className="font-medium px-4 text-gray-500 hover:text-gray-900">All Orders</a>
-        </Link>
-
+        {items.map((item, index) =>
+          <ActiveLink href={item.href} key={index}>
+            <a className="font-medium px-4 text-gray-500 hover:text-gray-900 text-lg">{item.value}</a>
+          </ActiveLink>
+        )}
       </ol>
     </nav>
   )
--- components/ui/marketplace/header/index.js
+++ components/ui/marketplace/header/index.js
@@ -1,6 +1,16 @@
 import { Breadcrumbs } from "@components/ui/common";
 import { EthRates, WalletBar } from "@components/ui/web3";
 
+const LINKS = [{
+    href: "/marketplace",
+    value: "Buy"
+},{
+    href: "/marketplace/courses/owned",
+    value: "My Courses"
+},{
+    href: "/marketplace/courses/manage",
+    value: "Manage Courses"
+}]
 
 export default function Header() {
 
@@ -8,7 +18,7 @@ export default function Header() {
         <>
             <WalletBar />
             <EthRates />
-            <Breadcrumbs />
+            <Breadcrumbs items={LINKS}/>
         </>
     )
 }
